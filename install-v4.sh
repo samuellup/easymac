b80a17a86aaedd3c488b2850f500c445cedcdb67
samuellup <slup@umh.es>
2017-11-28 09:18:59 +0100

    -

--- install-v4.sh
+++ install-v4.sh
@@ -98,13 +98,12 @@ echo "@reboot   root    cd $location; ./src/Python-2.7.12/.localpython/bin/pytho
 # Finally we check if Easymap functions properly by running a small project: 
 cp fonts/check.1.fa user_data/
 cp fonts/check.gff user_data/
-
 run_result=`./easymap -n setup -w snp -sim -r check -g check.gff -ed ref_bc_parmut`
 
 # Cleanup
 rm  user_data/check.gff
 rm  user_data/check.1.fa 
-#rm -rf user_projects/*
+rm -rf user_projects/*
 
 if [ "$run_result" == "Easymap analysis properly completed." ]; then
 	echo " "
