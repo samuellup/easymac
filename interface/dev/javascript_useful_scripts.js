2e94ec77df9c6949afb5b3764e2b982ddad26408
sergioSEa <dws1985@hotmail.com>
2017-11-30 17:52:05 +0100

    -

--- interface/dev/javascript_useful_scripts.js
+++
@@ -1,6 +0,0 @@
-// Show the user an alert that can be accepted or declined to perform alternative actions
-function promptUser(arg) {
-	if (confirm('Are you sure you want to do this?')) {
-		// do something
-	}
-}
